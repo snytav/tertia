417643fa373d9af3eababcdace9d5e349b306d51
snytav <snytav@gmail.com>
2025-05-03 16:06:57 +0200

    void attributes list

--- movepart_splitlayer.cpp
+++ movepart_splitlayer.cpp
@@ -1533,7 +1533,7 @@ void Mesh::MoveParticlesLayerSplit(int iLayer,int iSplit, int iFullStep, double 
                   int i256 = 0;		 
 	       }
 	       
-               CUDA_WRAP_write_plasma_value(np,PLASMA_VALUES_NUMBER,91,(double)(ncc_check - (l_dMy + l_sizeY *  l_dMz + 2*l_dMy*ktmp)));
+               CUDA_WRAP_write_plasma_value(np,PLASMA_VALUES_NUMBER,91,(double)(ncc_check- (l_dMy + l_sizeY *  l_dMz + 2*l_dMy*ktmp)));
 	       //printf("rank %d b_deposit %3d %3d %3d y %e z %e rho %e \n ",GetRank(),iLayer,j,k,xtmp,ytmp,drho*part_step);
                DepositCurrentsInCellSplit(p, isort, itmp, jtmp, ktmp, Vx, Vy, Vz, xtmp, ytmp, ztmp, 
                   djx*part_step, djy*part_step, djz*part_step, drho*part_step);
