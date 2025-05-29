9cb369dfa52d1cfd8072e352b0c2adc0511fe624
Alexey Snytnikov <snytav@gmail.com>
2025-05-29 23:27:30 +0200

    found exit

--- step.cpp
+++ step.cpp
@@ -92,7 +92,7 @@ int Domain::Step(void)
    CUDA_WRAP_copyBeamToArray(p_M,l_Xsize,l_Ysize,l_Zsize,p_M->p_CellArray,&d_RhoBeam3D,&d_JxBeam3D);
 //#endif   
    puts("  CUDA_WRAP_copyBeamToArray  ");
-   exit(0); 
+   //exit(0); 
    
    
 //////////////////////////////////////////////////////////////////t(0);   
