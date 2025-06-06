de1776a2a213189dbc4d1482e9f16f2c710d6624
snytav <snytav@gmail.com>
2025-06-06 18:05:54 +0200

    beam particles and fields control_point

---
+++ beam_control_point.cpp
@@ -0,0 +1,75 @@
+int CUDA_WRAP_printBeamDensity3D(Mesh *p_M,int step,char *where)
+{
+#ifdef CUDA_WRAP_BEAM_3D_DENSITY_PRINT
+    char fname[100];
+    FILE *f;
+    long Mx,My,Mz,dMx,dMy,dMz;
+
+    p_M->GetSizes(Mx,My,Mz,dMx,dMy,dMz);
+
+    sprintf(fname,"beam_%s_%03d_rank%03d.dat",where,step,GetRank());
+
+    f = fopen(fname,"wt");
+
+    for (int i = -dMx; i < Mx + dMx - 1; i++) {
+      for (int k = -dMz; k < Mz + dMz - 1; k++) {
+         for (int j = -dMy; j < My + dMy - 1; j++) {
+            Cell& ccc = p_M->GetCell(i,j,k);
+	    fprintf(f,"%3d %3d %3d %25.15e %25.15e\n",i,j,k,ccc.GetJxBeam(),ccc.GetRhoBeam());
+/*	    Cell &ccc_c =  p_CellLayerC[nYZ];
+	    Cell &ccc_p =  p_CellLayerC[nYZ];
+	    double *fds = ccc_p.GetFields();
+
+	    ccc_c.SetFields(fds);
+*/
+         }
+      }
+    }
+    fclose(f);
+#endif
+
+   return 0;
+}
+
+int CUDA_WRAP_printBeamParticles(Mesh *p_M,int step,char *where)
+{
+#ifdef CUDA_WRAP_BEAM_PARTICLES_PRINT
+    char fname[100];
+    FILE *f;
+    long Mx,My,Mz,dMx,dMy,dMz;
+
+    p_M->GetSizes(Mx,My,Mz,dMx,dMy,dMz);
+
+    sprintf(fname,"beamParticles_%s_%03d_rank%03d.dat",where,step,GetRank());
+
+    f = fopen(fname,"wt");
+
+    for (int i = -dMx; i < Mx + dMx - 1; i++) {
+      for (int k = -dMz; k < Mz + dMz - 1; k++) {
+         for (int j = -dMy; j < My + dMy - 1; j++) {
+            Cell& ccc = p_M->GetCell(i,j,k);
+	    Particle *p = ccc.GetBeamParticles();
+	    int num = 0;
+	    while(p)
+	    {
+	       fprintf(f,"%3d %3d %3d %d %25.15e %25.15e %25.15e %25.15e %25.15e %25.15e %25.15e %25.15e\n",i,j,k,num,p->f_X,p->f_Y,p->f_Z,p->f_Px,p->f_Pz,p->f_Pz,p->f_Weight,p->f_Q2m);
+
+	       p = p->p_Next;
+	       num++;
+	    }
+
+/*	    Cell &ccc_c =  p_CellLayerC[nYZ];
+	    Cell &ccc_p =  p_CellLayerC[nYZ];
+	    double *fds = ccc_p.GetFields();
+
+	    ccc_c.SetFields(fds);
+*/
+         }
+      }
+    }
+    fclose(f);
+#endif
+
+
+   return 0;
+}
