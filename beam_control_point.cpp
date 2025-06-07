b4477a78c4ec50dab7894e69ba3da1b1b5a17a9d
Alexey Snytnikov <snytav@gmail.com>
2025-06-07 13:03:13 +0200

    beam_MoveParticlesLayerSplit_0000000001.dat with E,B

--- beam_control_point.cpp
+++ beam_control_point.cpp
@@ -22,7 +22,21 @@ int printBeamDensity3D(Mesh *p_M,char *where)
       for (int k = -dMz; k < Mz + dMz - 1; k++) {
          for (int j = -dMy; j < My + dMy - 1; j++) {
             Cell& ccc = p_M->GetCell(i,j,k);
-	    fprintf(f,"%3d %3d %3d %25.15e %25.15e\n",i,j,k,ccc.GetJxBeam(),ccc.GetRhoBeam());
+	    fprintf(f,"i %3d j %3d k %3d JxBeam %25.15e RhoBeam %25.15e \
+	               Ex %25.15e Ey %25.15e Ez %25.15e  \
+	               Bx %25.15e By %25.15e Bz %25.15e  \
+	                \n",
+                i,j,k,
+                ccc.GetJxBeam(),
+                ccc.GetRhoBeam(),
+                ccc.GetEx(),
+                ccc.GetEy(),
+                ccc.GetEz(),
+                ccc.GetBx(),
+                ccc.GetBy(),
+                ccc.GetBz());
+
+
 /*	    Cell &ccc_c =  p_CellLayerC[nYZ];
 	    Cell &ccc_p =  p_CellLayerC[nYZ];
 	    double *fds = ccc_p.GetFields();
