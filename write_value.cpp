61260e972c6b7449a71d2b3cdbea4bacbf7a8981
Alexey Snytnikov <snytav@gmail.com>
2025-06-11 00:19:55 +0200

    rebuilding values system

---
+++ write_value.cpp
@@ -0,0 +1,20 @@
+#define PLASMA_VALUES_NUMBER 200
+#define MAX_PLASMA_PARTICLES 50000
+
+double *h_plasma_values;
+
+int CUDA_WRAP_write_plasma_value(int i,int num_attr,int n,double t)
+{
+	static int first = 1;
+    int cell_number = i*Ny + j;
+
+    if(first == 0)
+    {
+        h_plasma_values = (double*)malloc(PLASMA_VALUES_NUMBER*MAX_PLASMA_PARTICLES*sizeof(double));
+        first = 0;
+    }
+
+	h_plasma_values [i*num_attr + n] = t;
+
+
+	return 0;
