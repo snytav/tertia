a2c7f8ff07dd835ca1032970665a3e076a6c4e8b
Alexey Snytnikov <snytav@gmail.com>
2025-07-31 09:17:00 +0200

    POSITION in control

--- CUDA_WRAP/cuBeamValues.cu
+++ CUDA_WRAP/cuBeamValues.cu
@@ -148,8 +148,8 @@ double CUDA_WRAP_check_beam_values(int Np,int num_attr,double *h_p,double *d_p,i
 	   for (int i = 0;i < Np1;i++)
            {
 	
-            cu_x = h_copy[i];
-     	    x    = h_p   [i];
+            cu_x = h_copy[ POSITION(i,n,num_attr)    ];
+     	    x    = h_p   [ POSITION(i,n,num_attr)    ];
 			  
             fprintf(f_dump,"attr %10d np  %10d cpu %25.15e gpu %25.15e delta %15.5e \n",n,i,x,cu_x,fabs(cu_x - x));
            }
